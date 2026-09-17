59d3d1593dac3dec1a2c83f0688dbafd7d5128b8
Hi-thecrit <aarushcrits101@gmail.com>
2026-09-17 18:44:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const client = new Client({ checkUpdate: false });
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setStatus('dnd'); 
-    client.user.setActivity('Always Online', { type: 'PLAYING' });
 });
 
 // DO NOT paste your token here. Keep it exactly as process.env.DISCORD_TOKEN
